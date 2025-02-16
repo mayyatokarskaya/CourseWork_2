ba48d01ce4fd59e2f5d26f706825d418cc689d0f
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-02-16 20:37:25 +1000

    change main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
 from job_api import HeadHunterAPI
+from vacancy import Vacancy
+from json_saver import JSONSaver
 
 if __name__ == "__main__":
     hh_api = HeadHunterAPI()
-    search_query = input("Введите поисковый запрос: ")
-    vacancies = hh_api.get_vacancies(search_query, per_page=12)
+    json_saver = JSONSaver()
 
-    print("\nНайденные вакансии:")
-    for vacancy in vacancies:
-        print(vacancy["name"], "-", vacancy["alternate_url"])
+    search_query = input("Введите поисковый запрос: ")
+    vacancies_json = hh_api.get_vacancies(search_query, per_page=5)
+    vacancies = [Vacancy.from_dict(vac) for vac in vacancies_json]
+
+    print("\nСохранение вакансий в файл...")
+    json_saver.save_vacancies(vacancies)
+
+    print("\nЗагруженные вакансии из файла:")
+    loaded_vacancies = json_saver.load_vacancies()
+    for vac in loaded_vacancies:
+        print(vac)
+
+    keyword = input("\nВведите слово для фильтрации: ")
+    filtered_vacancies = json_saver.filter_vacancies(keyword)
+    print("\nОтфильтрованные вакансии:")
+    for vac in filtered_vacancies:
+        print(vac)
+
+
