73230211359e1dafc9d634de89eb133b9f96a025
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-03-02 19:36:06 +1000

    change files

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,7 +1,6 @@
 import json
 import os
-
-from vacancy import Vacancy
+from src.vacancy import Vacancy
 
 
 class JSONSaver:
@@ -10,19 +9,38 @@ class JSONSaver:
     FILE_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "vacancies.json")
 
     def __init__(self, file_path=None):
-        """Рарешает задать свой путь к файлу"""
+        """Позволяет задать свой путь к файлу"""
         self.file_path = file_path if file_path else self.FILE_PATH
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)  # Создаём папку, если её нет
+
+    # def save_vacancies(self, vacancies):
+    #     """Сохраняет список вакансий в JSON-файл"""
+    #     data = [vac.__dict__ for vac in vacancies]
+    #     with open(self.file_path, "w", encoding="utf-8") as file:
+    #         json.dump(data, file, ensure_ascii=False, indent=4)
 
     def save_vacancies(self, vacancies):
         """Сохраняет список вакансий в JSON-файл"""
         os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
-        data = [vac.__dict__ for vac in vacancies]
+        data = [
+            {
+                "title": vac.title,
+                "url": vac.url,
+                "salary_from": vac.salary_from,
+                "salary_to": vac.salary_to,
+                "description": vac.description,
+            }
+            for vac in vacancies
+        ]
         with open(self.file_path, "w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
+
+
+
     def load_vacancies(self):
         """Загружает вакансии из JSON-файла"""
-        if not os.path.exists(self.file_path):
+        if not os.path.exists(self.file_path) or os.stat(self.file_path).st_size == 0:
             return []
 
         with open(self.file_path, "r", encoding="utf-8") as file:
@@ -42,6 +60,6 @@ class JSONSaver:
         self.save_vacancies(vacancies)
 
     def filter_vacancies(self, keyword):
-        """Фильтрует вакансии по ключевому слову"""
+        """Фильтрует вакансии по ключевому слову (без учёта регистра)"""
         vacancies = self.load_vacancies()
         return [vac for vac in vacancies if keyword.lower() in vac.description.lower()]
